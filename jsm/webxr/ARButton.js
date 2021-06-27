e75e69c6fe21fc81ad33264b7f521ae9551d5e82
anibs <79148333+anibs@users.noreply.github.com>
2021-06-27 23:01:51 +0530

    Update ARButton.js

--- jsm/webxr/ARButton.js
+++ jsm/webxr/ARButton.js
@@ -77,7 +77,7 @@ class ARButton {
 			button.style.display = '';
 
 			button.style.cursor = 'pointer';
-			button.style.left = 'calc(50% - 50px)';
+			button.style.right='20px';
 			button.style.width = '100px';
 
 			button.textContent = 'START AR';
@@ -115,7 +115,7 @@ class ARButton {
 			button.style.display = '';
 
 			button.style.cursor = 'auto';
-			button.style.left = 'calc(50% - 75px)';
+			button.style.right='20px';
 			button.style.width = '150px';
 
 			button.onmouseenter = null;
